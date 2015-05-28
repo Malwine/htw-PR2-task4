fadcd465cf6d8283bedd52143861f2c32808804e
Malwine <malwine@mailbox.org>
2015-05-28 23:09:18 +0200

    Add all buttons

--- src/ss2015_aufgabe4/Calculator.java
+++ src/ss2015_aufgabe4/Calculator.java
@@ -2,6 +2,7 @@ package ss2015_aufgabe4;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -18,7 +19,8 @@ import javax.swing.JPanel;
 public class Calculator extends JFrame implements ActionListener {
 
 	JPanel buttonPanel;
-	JButton num1, num2, num3, num4, num5, num6, num7, num8, num9, num0;
+	JButton plus, minus, divide, multiply, equals, openBracket, closingBracket, clear, dot,
+	  num1, num2, num3, num4, num5, num6, num7, num8, num9, num0;
 	JLabel textLabel;
 	
 	Calculator() {
@@ -26,15 +28,16 @@ public class Calculator extends JFrame implements ActionListener {
 		this.setTitle("Handle action");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		textLabel = new JLabel("Text");
+		textLabel = new JLabel("");
 		textLabel.setHorizontalAlignment(JLabel.CENTER);
 		textLabel.setFont(new Font("Verdana", Font.BOLD, 24));
 		this.add(textLabel, BorderLayout.NORTH);	     //add label to content pane
 		
 		buttonPanel = initButtonPanel();
+		buttonPanel.setLayout(new GridLayout(8,3));
 		this.add(buttonPanel, BorderLayout.SOUTH);
 		
-		this.setSize(800,350);
+		this.setSize(600,350);
 		this.setVisible(true);
 	}
 	
@@ -50,7 +53,16 @@ public class Calculator extends JFrame implements ActionListener {
 		num7 = new JButton("7");
 		num8 = new JButton("8");
 		num9 = new JButton("9");
+		openBracket = new JButton("(");
 		num0 = new JButton("0");
+		closingBracket = new JButton(")");
+		plus = new JButton("+");
+		minus = new JButton("-");
+		divide = new JButton("/");
+		multiply = new JButton("*");
+		dot = new JButton(".");
+		equals = new JButton("=");
+		clear = new JButton("C");
 		
 		num1.addActionListener(this);
 		num2.addActionListener(this);
@@ -61,7 +73,16 @@ public class Calculator extends JFrame implements ActionListener {
 		num7.addActionListener(this);
 		num8.addActionListener(this);
 		num9.addActionListener(this);
+		openBracket.addActionListener(this);
 		num0.addActionListener(this);
+		closingBracket.addActionListener(this);
+		plus.addActionListener(this);
+		minus.addActionListener(this);
+		divide.addActionListener(this);
+		multiply.addActionListener(this);
+		dot.addActionListener(this);
+		equals.addActionListener(this);
+		clear.addActionListener(this);
 		
 		panel.add(num1);
 		panel.add(num2);
@@ -72,9 +93,30 @@ public class Calculator extends JFrame implements ActionListener {
 		panel.add(num7);
 		panel.add(num8);
 		panel.add(num9);
+		panel.add(openBracket);
 		panel.add(num0);
+		panel.add(closingBracket);
+		panel.add(plus);
+		panel.add(minus);
+		panel.add(divide);
+		panel.add(multiply);
+		panel.add(dot);
+		panel.add(equals);
+		panel.add(clear);
 		
 		return panel;
+	}
+	
+	void changeLabel(String input){
+		
+		String content = this.textLabel.getText();
+		
+		if (content == null) {
+			this.textLabel.setText(input);
+		}
+		else {
+			this.textLabel.setText(content + input);
+		}
 	}
 
 	@Override
@@ -86,34 +128,37 @@ public class Calculator extends JFrame implements ActionListener {
 			
 			switch(e.getActionCommand()) {
 			case "1": 
-				this.textLabel.setText("1");
+				changeLabel("1");
 				break;
 			case "2": 
-				this.textLabel.setText("2");
+				changeLabel("2");
 				break;
 			case "3": 
-				this.textLabel.setText("3");
+				changeLabel("3");
 				break;
 			case "4": 
-				this.textLabel.setText("4");
+				changeLabel("4");
 				break;
 			case "5": 
-				this.textLabel.setText("5");
+				changeLabel("5");
 				break;
 			case "6": 
-				this.textLabel.setText("6");
+				changeLabel("6");
 				break;
 			case "7": 
-				this.textLabel.setText("7");
+				changeLabel("7");
 				break;
 			case "8": 
-				this.textLabel.setText("8");
+				changeLabel("8");
 				break;
 			case "9": 
-				this.textLabel.setText("9");
+				changeLabel("9");
 				break;
 			case "0": 
-				this.textLabel.setText("0");
+				changeLabel("0");
+				break;
+			case "C": 
+				this.textLabel.setText("");
 				break;
 			}
 		} 
